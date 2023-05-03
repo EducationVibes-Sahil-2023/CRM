695cf09b2f034912488fd39ee407a5052fb71b30
unknown <csahil3132@gmail.com>
2023-05-03 11:06:32 +0530

    update changes

--- application/config/config.php
+++ application/config/config.php
@@ -579,4 +579,5 @@ $config['csrf_exclude_uris'] = array_merge($config['csrf_exclude_uris'],array('f
 $config['csrf_exclude_uris'] = array_merge($config['csrf_exclude_uris'],array('facebook_leads_integration/new_webhook'));
 $config['csrf_exclude_uris'] = array_merge($config['csrf_exclude_uris'],array('facebook_leads_integration/new_webhook_test'));
 $config['csrf_exclude_uris'] = array_merge($config['csrf_exclude_uris'], array('external/login'));
+
 $config['csrf_exclude_uris'] = array_merge($config['csrf_exclude_uris'], array('external/followup_contact'));
