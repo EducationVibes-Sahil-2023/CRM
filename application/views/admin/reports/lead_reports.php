9dd5fef3e1b2c17acb17e8c56749a25c9a74ccec
EducationVibes-Sahil-2023 <131950247+EducationVibes-Sahil-2023@users.noreply.github.com>
2023-05-20 10:47:23 +0530

    Update lead_reports.php

--- application/views/admin/reports/lead_reports.php
+++ application/views/admin/reports/lead_reports.php
@@ -163,9 +163,13 @@
                         </div>
 
                         <div class="col-md-2 leads-filter-column">
+                             <?php
                             echo '<div id="leads-filter-source">';
                             echo render_select('lead_type[]', $type, array('id', 'name'), '', '', array('data-width' => '100%', 'data-none-selected-text' => _l('lead_import_type'), 'multiple' => true, 'data-actions-box' => true), array(), 'no-mbot', '', false, "lead_type");
                             echo '</div>';
+
+                            // die;
+                            ?>
 
                         </div>
                         <div class="col-md-2 leads-filter-column">
