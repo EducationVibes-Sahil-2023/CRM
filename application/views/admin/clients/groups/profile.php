974cafba72672e83299b078bbc07a701c61de942
unknown <csahil3132@gmail.com>
2023-05-23 09:56:30 +0530

    update custom field

--- application/views/admin/clients/groups/profile.php
+++ application/views/admin/clients/groups/profile.php
@@ -199,6 +199,9 @@
                   $selected =( isset($client) ? $client->country : $customer_default_country);
                   echo render_select( 'country',$countries,array( 'country_id',array( 'short_name')), 'clients_country',$selected,array('data-none-selected-text'=>_l('dropdown_non_selected_tex')));
                   ?>
+				  <?php  
+				  echo render_custom_fields('customers', $client->userid);
+				   ?>
             </div>
          </div>
       </div>
