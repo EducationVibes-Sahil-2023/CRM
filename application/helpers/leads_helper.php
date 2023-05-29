d3732f0254ae297278ebca337017e5ed214e971b
EducationVibes-Sahil-2023 <131950247+EducationVibes-Sahil-2023@users.noreply.github.com>
2023-05-29 19:05:27 +0530

    Update leads_helper.php

--- application/helpers/leads_helper.php
+++ application/helpers/leads_helper.php
@@ -683,13 +683,10 @@ function leads_update_count($params = false, $max_status = 0)
 
 
     // $sql = "SELECT SUM(total) as total_sum FROM ( {$sql} )  as subquery ";
-    echo $sql = trim($sql);
-
-die;
-
+     $sql = trim($sql);
     // $update_count = $CI->db->query($sql)->row()->total_sum;
     $update_count = $CI->db->query($sql)->result_array();
-	print_r($update_count);
+print_r($update_count);
 	die;
     // // Extract the 'id' column
     // $ids = array_column($update_count, 'id');
