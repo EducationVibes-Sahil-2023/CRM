ce93c8df19eb18f3ab3f6d118e9452bebd2caa1c
EducationVibes-Sahil-2023 <131950247+EducationVibes-Sahil-2023@users.noreply.github.com>
2023-06-18 11:20:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -171,9 +171,6 @@ switch (ENVIRONMENT)
 			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
 
 		}
-ini_set ('display_errors', 1);
-ini_set ('display_startup_errors', 1);
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
 	break;
 
 
