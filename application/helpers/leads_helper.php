f525a24bd194158a8df5d08eb5f82de266377892
unknown <csahil3132@gmail.com>
2023-06-21 21:28:24 +0530

    update changes

--- application/helpers/leads_helper.php
+++ application/helpers/leads_helper.php
@@ -1275,7 +1275,7 @@ function calls_update_count($params = false, $max_status = 0)
     // $sql .= ' SELECT COUNT(l.id) as total';
     // $sql .= ' SELECT count(distinct(CAST(n.dateadded AS date))) as total';
 
-    $sql .= "SELECT sum(calls.duration) call_duration FROM tblleads AS l INNER JOIN   " . db_prefix() . "calls_activity_logs AS calls ON FIND_IN_SET(RIGHT(TRIM(l.phonenumber), 10), (SELECT GROUP_CONCAT(DISTINCT RIGHT(TRIM(contact), 10)) FROM " . db_prefix() . "calls_activity_logs WHERE LOWER(TRIM(call_status)) IN ('answered', 'status_unknow'))) > 0 left join " . db_prefix() . "notes n ON ( l.id = n.rel_id   ";
+    $sql .= "SELECT sum(distinct calls.duration) call_duration FROM tblleads AS l INNER JOIN   " . db_prefix() . "calls_activity_logs AS calls ON FIND_IN_SET(RIGHT(TRIM(l.phonenumber), 10), (SELECT GROUP_CONCAT(DISTINCT RIGHT(TRIM(contact), 10)) FROM " . db_prefix() . "calls_activity_logs WHERE LOWER(TRIM(call_status)) IN ('answered', 'status_unknow'))) > 0 left join " . db_prefix() . "notes n ON ( l.id = n.rel_id   ";
 
     if (!empty($params['up_to_date'])) {
         $up_from_date = $params['up_from_date'];
