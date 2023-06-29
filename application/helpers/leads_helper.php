d74233761b63a870e75f7965bedd0857ca95f04a
unknown <csahil3132@gmail.com>
2023-06-29 19:58:17 +0530

    update calls

--- application/helpers/leads_helper.php
+++ application/helpers/leads_helper.php
@@ -1359,8 +1359,8 @@ function calls_update_count($params = false, $max_status = 0)
     } else if (!empty($params['up_to_date'])) {
         $up_from_date = $params['up_from_date'];
         $up_to_date = $params['up_to_date'];
-        $sql .= ' AND (  (DATE(l.lastcontact) BETWEEN "' . $CI->db->escape_str($up_from_date) . '" AND "' . $CI->db->escape_str($up_to_date) . '") ) ';
-        // $sql .= "  OR ( FROM_UNIXTIME(calls.call_start) between '{$up_from_date}' AND '{$up_to_date}' ) ) ";
+        $sql .= ' AND (  (DATE(l.lastcontact) BETWEEN "' . $CI->db->escape_str($up_from_date) . '" AND "' . $CI->db->escape_str($up_to_date) . '") ';
+        $sql .= "  OR ( FROM_UNIXTIME(calls.call_start) between '{$up_from_date}' AND '{$up_to_date}' ) ) ";
     }/*else{
             $today = date("Y-m-d");
             $sql .= " AND n.dateadded LIKE '%" .$today."%'";
