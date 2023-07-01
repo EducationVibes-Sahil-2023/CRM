732bca9f67ae344947d92cdd940303d1400a4e17
unknown <csahil3132@gmail.com>
2023-07-01 12:39:43 +0530

    update calls

--- application/helpers/leads_helper.php
+++ application/helpers/leads_helper.php
@@ -1285,7 +1285,7 @@ function calls_update_count($params = false, $max_status = 0)
     if (!empty($params['up_to_date'])) {
         $up_from_date = $params['up_from_date'];
         $up_to_date = $params['up_to_date'];
-        $sql .= ' AND DATE(n.dateadded) BETWEEN "' . $CI->db->escape_str($up_from_date) . '" AND "' . $CI->db->escape_str($up_to_date) . '"';
+        // $sql .= ' AND DATE(n.dateadded) BETWEEN "' . $CI->db->escape_str($up_from_date) . '" AND "' . $CI->db->escape_str($up_to_date) . '"';
     }
     $sql .= " ) ";
 
