043cb4067758edb47eb28d248cfd728fe674435e
EducationVibes-Sahil-2023 <131950247+EducationVibes-Sahil-2023@users.noreply.github.com>
2023-07-19 14:28:46 +0530

    Update Forms.php

--- application/controllers/Forms.php
+++ application/controllers/Forms.php
@@ -99,19 +99,23 @@ class Forms extends ClientsController
                         $status_fb_lead_assign = false;
                         if (!empty($assign_staff_id)) {
                             foreach ($assign_staff_id as $fl) {
-                                if (!empty($fl["facebook_lead_name"])) {
-                                    $fb_form_name = explode(",", $fl["facebook_lead_name"]);
-                                    if (!empty($fb_form_name)) {
-                                        foreach ($fb_form_name as $fb_name) {
-                                            if (!empty($fb_name)) {
-                                                if (strpos(strtolower(trim($facebook_lead_name)), strtolower(trim($fb_name))) !== false) {
-                                                    $form->responsible = $fl["staffid"];
-                                                    $status_fb_lead_assign = true;
-                                                    break;
-                                                }
-                                            }
-                                        }
-                                    }
+
+                                  $form->responsible = $fl["staffid"];
+                                    $status_fb_lead_assign = true;
+                                    break;
+                                // if (!empty($fl["facebook_lead_name"])) {
+                                //     $fb_form_name = explode(",", $fl["facebook_lead_name"]);
+                                //     if (!empty($fb_form_name)) {
+                                //         foreach ($fb_form_name as $fb_name) {
+                                //             if (!empty($fb_name)) {
+                                //                 if (strpos(strtolower(trim($facebook_lead_name)), strtolower(trim($fb_name))) !== false) {
+                                //                     $form->responsible = $fl["staffid"];
+                                //                     $status_fb_lead_assign = true;
+                                //                     break;
+                                //                 }
+                                //             }
+                                //         }
+                                //     }
                                 }
                             }
                         }
