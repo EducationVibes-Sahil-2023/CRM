f826dee63dbbba3e7a98491014714ddd73de851f
unknown <csahil3132@gmail.com>
2023-08-09 15:57:17 +0530

    changes

--- application/helpers/datatables_helper.php
+++ application/helpers/datatables_helper.php
@@ -212,14 +212,10 @@ function data_tables_init($aColumns, $sIndexColumn, $sTable, $join = [], $where 
                     }
                     if (count($additionalSelect) > 0) {
                         foreach ($additionalSelect as $searchAdditionalField) {
-<<<<<<< HEAD
                             // $searchAdditionalField = explode(" ", $searchAdditionalField)[0];
                             if (strpos($searchAdditionalField, ' as ') !== false) {
                                 $searchAdditionalField = strbefore($searchAdditionalField, ' as');
                             }
-=======
-                            $searchAdditionalField = explode(" ", $searchAdditionalField)[0];
->>>>>>> 5953cfe7584057a1ac853758043b103c3bdac75b
                             if (str_contains($search_value, '!=')) {
                                 $sWhere .= 'convert(ifnull(' . $searchAdditionalField . ',"") USING utf8)' . " NOT LIKE '" . $CI->db->escape_str(str_replace("!=", "", $search_value)) . "%' AND ";
                             } else {
