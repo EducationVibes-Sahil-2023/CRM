93b385be11cb5fee774673058c8e9717b70184ce
EducationVibes-Sahil-2023 <131950247+EducationVibes-Sahil-2023@users.noreply.github.com>
2023-08-20 14:10:52 +0530

    Update Forms.php

--- application/controllers/Forms.php
+++ application/controllers/Forms.php
@@ -822,23 +822,70 @@ class Forms extends ClientsController
 
 
 
+    // private function curl_function($post_data)
+    // {
+    //     $data = array("call_data" => json_encode($post_data));
+    //     try {
+    //         $token = JWT_TOKEN;
+    //         header('Content-Type: application/json'); // Specify the type of data
+    //         $ch = curl_init(base_url("external/call_update")); // Initialise cURL
+    //         $authorization = "Authorization: Bearer " . $token; // Prepare the authorisation token
+    //         curl_setopt($ch, CURLOPT_HTTPHEADER, array($authorization)); // Inject the token into the header
+    //         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //         curl_setopt($ch, CURLOPT_POST, 1); // Specify the request method as POST
+    //         curl_setopt($ch, CURLOPT_POSTFIELDS, $data); // Set the posted fields
+    //         // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // This will follow any redirects
+    //         $result = curl_exec($ch); // Execute the cURL statement
+    //         curl_close($ch); // Close the cURL connection
+    //     } catch (Exception $e) {
+    //         return true;
+    //     }
+    // }
+
     private function curl_function($post_data)
-    {
-        $data = array("call_data" => json_encode($post_data));
-        try {
-            $token = JWT_TOKEN;
-            header('Content-Type: application/json'); // Specify the type of data
-            $ch = curl_init(base_url("external/call_update")); // Initialise cURL
-            $authorization = "Authorization: Bearer " . $token; // Prepare the authorisation token
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array($authorization)); // Inject the token into the header
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POST, 1); // Specify the request method as POST
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data); // Set the posted fields
-            // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // This will follow any redirects
-            $result = curl_exec($ch); // Execute the cURL statement
-            curl_close($ch); // Close the cURL connection
-        } catch (Exception $e) {
-            return true;
+{
+    $data = array("call_data" => json_encode($post_data));
+    
+    try {
+        $token = JWT_TOKEN;
+        $url = base_url("external/call_update");
+        
+        $ch = curl_init($url);
+        
+        if ($ch === false) {
+            throw new Exception('Failed to initialize cURL');
         }
+
+        $authorization = "Authorization: Bearer " . $token;
+        $headers = array('Content-Type: application/json', $authorization);
+        
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+
+        $result = curl_exec($ch);
+
+        if ($result === false) {
+            throw new Exception('cURL error: ' . curl_error($ch));
+        }
+
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        
+        if ($http_code !== 200) {
+            throw new Exception('HTTP error: ' . $http_code);
+        }
+
+        curl_close($ch);
+        
+        // Return the result or handle it as needed.
+        return $result;
+    } catch (Exception $e) {
+        // Handle the error here, e.g., log the error message or take appropriate action.
+        // You can also echo or return the error message for debugging purposes.
+        echo "Error: " . $e->getMessage();
+        return false;
     }
+}
+
 }
