a36cd1ec385b764f0a58de79512dee995f51448a
EducationVibes-Sahil-2023 <131950247+EducationVibes-Sahil-2023@users.noreply.github.com>
2023-08-20 19:32:32 +0530

    Update Forms.php

--- application/controllers/Forms.php
+++ application/controllers/Forms.php
@@ -932,4 +932,14 @@ if (!empty($call_data)) {
         return json_encode($result);
     }
 
+        public function test_db_connection()
+    {
+        $response = $this->db->select("*")->from(db_prefix() . "leads")->get()->result_array();
+        echo json_encode($response);
+    }
+    public function test_code()
+    {
+        echo json_encode(array("success" => 1, "message" => "Run Successfully"));
+    }
+
 }
