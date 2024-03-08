2a9a803b3a873fb2ed27f842dd39db4df4771348
unknown <csahil3132@gmail.com>
2024-03-08 16:16:08 +0530

    update changes

--- application/config/config.php
+++ application/config/config.php
@@ -329,7 +329,7 @@ $config['cache_path'] = '';
 |                of query parameters.
 |
 */
-$config['cache_query_string'] = true;
+$config['cache_query_string'] = false;
 
 /*
 |--------------------------------------------------------------------------
