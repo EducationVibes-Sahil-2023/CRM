acfedee960e408eaab606f1a795ba67dc1c82689
unknown <csahil3132@gmail.com>
2024-07-19 11:49:40 +0530

    update changes assects

--- application/views/admin/staff/member.php
+++ application/views/admin/staff/member.php
@@ -418,6 +418,8 @@
                      </div>
                      <?php if (!empty($member->staffid)) { ?>
                         <div role="tabpanel" class="tab-pane" id="staff_assets">
+                           <h4>Assects Allocation</h4>
+                           <hr>
                            <?php
                            $table_data = array(
                               _l('time'),
@@ -429,6 +431,23 @@
                               _l('acction_to'),
                            );
                            render_datatable($table_data, 'table_action');
+                           ?>
+                           <br>
+                           <hr>
+                           <br>
+                           <h4>Assects Revoke</h4>
+                           <hr>
+                           <?php
+                           $table_data = array(
+                              _l('time'),
+                              _l('asset_name'),
+                              _l('acction_code'),
+                              _l('action'),
+                              _l('quantity_as_qty'),
+                              _l('acction_from'),
+                              _l('acction_to'),
+                           );
+                           render_datatable($table_data, 'table_action_revoke');
                            ?>
                         </div>
                      <?php } ?>
@@ -785,6 +804,7 @@
       }
 
       initDataTable('.table-table_action', admin_url + 'assets/table_action_allocate/allocation?action_to=<?= isset($member) ? $member->staffid : null ?>');
+      initDataTable('.table-table_action_revoke', admin_url + 'assets/table_action_allocate/revoke?action_to=<?= isset($member) ? $member->staffid : null ?>');
    </script>
    </body>
 
