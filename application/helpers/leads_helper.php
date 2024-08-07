21e6237b2af79d3293790bf42a8e0c0c7bc35e03
unknown <csahil3132@gmail.com>
2024-08-07 13:09:18 +0530

    update changes

--- application/helpers/leads_helper.php
+++ application/helpers/leads_helper.php
@@ -2616,7 +2616,7 @@ function get_leads_report_conversion($params)
         } elseif ($params['date_type'] == "week") {
             $sql .= "GROUP BY YEARWEEK(l.dateadded, 1), c.name ORDER BY YEARWEEK(l.dateadded, 1) ASC";
         } elseif ($params['date_type'] == "month") {
-            $sql .= "GROUP BY DATE_FORMAT(l.dateadded, '%Y - %M'), c.name ORDER BY DATE_FORMAT(l.dateadded, '%M - %Y') ASC";
+            $sql .= "GROUP BY DATE_FORMAT(l.dateadded, '%Y - %M'), c.name ORDER BY DATE_FORMAT(l.dateadded, '%Y-%m') ASC";
         } elseif ($params['date_type'] == "year") {
             $sql .= "GROUP BY YEAR(l.dateadded), c.name ORDER BY YEAR(l.dateadded) ASC";
         }
@@ -2719,7 +2719,7 @@ function get_leads_report_marketing($params)
         } elseif ($params['date_type'] == "week") {
             $sql .= "GROUP BY YEARWEEK(l.dateadded, 1), m.name ORDER BY YEARWEEK(l.dateadded, 1) ASC";
         } elseif ($params['date_type'] == "month") {
-            $sql .= "GROUP BY DATE_FORMAT(l.dateadded, '%Y - %M'), m.name ORDER BY DATE_FORMAT(l.dateadded, '%M - %Y') ASC";
+            $sql .= "GROUP BY DATE_FORMAT(l.dateadded, '%Y - %M'), m.name ORDER BY DATE_FORMAT(l.dateadded, '%Y-%m') ASC";
         } elseif ($params['date_type'] == "year") {
             $sql .= "GROUP BY YEAR(l.dateadded), m.name ORDER BY DATE_FORMAT(l.dateadded, '%Y') ASC";
         }
