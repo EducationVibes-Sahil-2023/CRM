d67aec8c5c90950a516ddcc707dc95618e5cf78c
unknown <csahil3132@gmail.com>
2025-04-07 16:09:18 +0530

    update changes

--- application/views/admin/clients/mbbs_abroad_manage.php
+++ application/views/admin/clients/mbbs_abroad_manage.php
@@ -759,7 +759,7 @@ init_tail(); ?>
 
 
 
-      applicant_table = initDataTable('.table-clients', admin_url + 'clients/table/2', [0], [0], CustomersServerParams, <?php echo hooks()->apply_filters('customers_table_default_order', json_encode(array(2, 'asc'))); ?>);
+      applicant_table = initDataTable('.table-clients', admin_url + 'clients/table/2', [0], [0], CustomersServerParams, []);
 
       disabled_column();
 
