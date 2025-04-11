8cc7101e6ab2a7a1f45a77194859bec87ae64a73
unknown <csahil3132@gmail.com>
2025-04-11 17:40:07 +0530

    update changes

--- application/views/admin/tables/mbbs_abroad_clients.php
+++ application/views/admin/tables/mbbs_abroad_clients.php
@@ -286,7 +286,13 @@ if (is_admin() || is_postSale()) {
     }
 }
 
-$result = data_tables_init(array_merge($aColumns, $additional_array), $sIndexColumn, $sTable, $join, $where, [], 'GROUP BY ' . db_prefix() . 'clients.userid');
+$search_column = [];
+// Define search and group-by clauses
+if (!empty($_POST["search"]["value"])) {
+    $search_column = [db_prefix() . "basic_details.email", db_prefix() . "basic_details.mobile", db_prefix() . "basic_details.first_name", db_prefix() . "basic_details.last_name"];
+}
+
+$result = data_tables_init(array_merge($aColumns, $additional_array), $sIndexColumn, $sTable, $join, $where, [], 'GROUP BY ' . db_prefix() . 'clients.userid', '', '', $search_column);
 
 $output  = $result['output'];
 $rResult = $result['rResult'];
