41af0524e472bc72392210608207b1ce61d90c24
unknown <csahil3132@gmail.com>
2025-06-03 14:40:37 +0530

    update changes

--- application/views/admin/clients/groups/ev_partner_profile.php
+++ application/views/admin/clients/groups/ev_partner_profile.php
@@ -360,7 +360,7 @@ if ($lead_type_status == 2) {
 										<div class="col-lg-3">
 											<div class="form-group">
 												<label for="exampleInputDateOfBirth">Date Of Birth <small class="text-danger"></small></label>
-												<input type="date" class="form-control" name="dob" id="dob" required value='<?php echo ($basicdetails->dob != '') ? $basicdetails->dob : ''; ?>' required required-check>
+												<input type="date" class="form-control" name="dob" id="dob" required value='<?php echo ($basicdetails->dob != '') ? $basicdetails->dob : ''; ?>' >
 											</div>
 										</div>
 
@@ -368,7 +368,7 @@ if ($lead_type_status == 2) {
 										<div class="col-lg-3">
 											<div class="form-group">
 												<label for="exampleInputPassword1">Gender <small class="text-danger"></small></label>
-												<select class="form-control" name="gender" id="gender" required required-check>
+												<select class="form-control" name="gender" id="gender" >
 													<option value="">Select</option>
 													<option <?php echo ($basicdetails->gender == 'Male') ? 'selected' : ''; ?>>Male</option>
 													<option <?php echo ($basicdetails->gender == 'Female') ? 'selected' : ''; ?>>Female</option>
