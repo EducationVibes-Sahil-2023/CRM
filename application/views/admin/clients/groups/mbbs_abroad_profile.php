3574bf614ff65218cf9d6067d2aea009273e5c68
EducationVibes-Sahil-2023 <sahil.chaudhary@educationvibes.in>
2025-07-28 10:51:00 +0530

    updte

--- application/views/admin/clients/groups/mbbs_abroad_profile.php
+++ application/views/admin/clients/groups/mbbs_abroad_profile.php
@@ -1224,7 +1224,7 @@ if ($lead_type_status == 2) {
 
 
                             </div>
-                            <div class="row btn-save-fun margin-top">
+                            <div class="row  margin-top">
                                 <div class="col-md-12">
                                     <button type="submit" onclick="save_documents()" class="btn btn-primary button-22 pull-right margin-top">Save changes</button>
                                 </div>
