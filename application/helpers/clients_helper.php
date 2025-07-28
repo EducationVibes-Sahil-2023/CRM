bf3489124b97e14f038ed312f47b64070f067cda
EducationVibes-Sahil-2023 <sahil.chaudhary@educationvibes.in>
2025-07-28 11:12:02 +0530

    update changes filter

--- application/helpers/clients_helper.php
+++ application/helpers/clients_helper.php
@@ -2852,12 +2852,15 @@ function filter_country_university_array($leadType)
 
     $CI->db->select('s.country_name, s.university_name, s.country_id, s.university_id,st.staffid,st.firstname,st.lastname,t.id source_id,t.name source_name');
     $CI->db->from(db_prefix() . 'clients c');
-    $CI->db->join(db_prefix() . 'leads l', 'c.leadid = l.id');
-    $CI->db->join(db_prefix() . 'client_university_shortlisting s', 'c.userid = s.client_id');
-    $CI->db->join(db_prefix() . 'staff st', 'c.addedfrom = st.staffid');
-    $CI->db->join(db_prefix() . 'leads_sources t', 'l.source = t.id');
+    $CI->db->join(db_prefix() . 'leads l', 'c.leadid = l.id', "LEFT");
+    $CI->db->join(db_prefix() . 'client_university_shortlisting s', 'c.userid = s.client_id', "LEFT");
+    $CI->db->join(db_prefix() . 'staff st', 'c.addedfrom = st.staffid', "LEFT");
+    $CI->db->join(db_prefix() . 'leads_sources t', 'l.source = t.id', "LEFT");
     $CI->db->where('l.type', $leadType);
-    $CI->db->where('s.status', 1);
+    if ($leadType == 1) {
+        $CI->db->or_where('c.client_type ', 2);
+    }
+
     $query = $CI->db->get();
     $result = $query->result_array();
 
